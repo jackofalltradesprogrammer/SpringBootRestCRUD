a83a16006878f5626de2b549c51fef66511628b7
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-05-02 14:38:43 -0400

    Add updateSave, delete, findById and aslo create a service layer

--- cruddemo/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAOHibernateImpl.java
+++ cruddemo/src/main/java/com/luv2code/springboot/cruddemo/dao/EmployeeDAOHibernateImpl.java
@@ -14,10 +14,10 @@ import com.luv2code.springboot.cruddemo.entity.Employee;
 
 @Repository
 public class EmployeeDAOHibernateImpl implements EmployeeDAO {
-	
+
 	// define field for entitymanager
 	private EntityManager entityManager;
-	
+
 	// set up constructor injection
 	@Autowired
 	public EmployeeDAOHibernateImpl(EntityManager entityManager) {
@@ -26,19 +26,47 @@ public class EmployeeDAOHibernateImpl implements EmployeeDAO {
 	}
 
 	@Override
-	@Transactional
 	public List<Employee> findAll() {
 
 		// get the hibernate session
 		Session currentSession = entityManager.unwrap(Session.class);
-		
+
 		// create the query
 		Query<Employee> theQuery = currentSession.createQuery("from Employee", Employee.class);
-		
+
 		// execute query and result list
 		List<Employee> employees = theQuery.getResultList();
 		return employees;
 	}
 
-	
+	@Override
+	public Employee findById(int theId) {
+
+		// get the hibernate session
+		Session currentSession = entityManager.unwrap(Session.class);
+
+		// get the employee
+		Employee theEmployee = currentSession.get(Employee.class, theId);
+		return theEmployee;
+	}
+
+	@Override
+	public void save(Employee theEmployee) {
+
+		// get the hibernate session
+		Session currentSession = entityManager.unwrap(Session.class);
+		// save employee
+		currentSession.saveOrUpdate(theEmployee);
+	}
+
+	@Override
+	public void deleteById(int theId) {
+
+		// get the hibernate session
+		Session currentSession = entityManager.unwrap(Session.class);
+		Query<Employee> theQuery = currentSession.createQuery("delete from Employee where id=:employeeId", Employee.class);
+		theQuery.setParameter("employeeId", theId);
+		theQuery.executeUpdate();
+	}
+
 }
