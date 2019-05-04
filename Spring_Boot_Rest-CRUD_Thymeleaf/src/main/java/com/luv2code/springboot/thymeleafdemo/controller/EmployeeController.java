56695e6ac4bb5456da2d0c3c2642f0058781cce1
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-05-03 21:00:48 -0400

    The controller use service to fetch employees from the database

--- Spring_Boot_Rest-CRUD_Thymeleaf/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
+++ Spring_Boot_Rest-CRUD_Thymeleaf/src/main/java/com/luv2code/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -1,41 +1,29 @@
 package com.luv2code.springboot.thymeleafdemo.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.annotation.PostConstruct;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.luv2code.springboot.thymeleafdemo.entity.Employee;
+import com.luv2code.springboot.thymeleafdemo.service.EmployeeService;
 
 @Controller
 @RequestMapping("/employees")
 public class EmployeeController {
 	
-	// load employee data
-	private List<Employee> theEmployees;
+	private EmployeeService employeeService;
 	
-	@PostConstruct
-	private void loadData() {
-		
-		Employee emp1 = new Employee(1, "Leslie", "Andrews", "leslie@luv2code.com");
-		Employee emp2 = new Employee(3, "Leslie", "Andrews", "leslie@luv2code.com");
-		Employee emp3 = new Employee(4, "Leslie", "Andrews", "leslie@luv2code.com");
-		theEmployees = new ArrayList<Employee>();
-		theEmployees.add(emp1);
-		theEmployees.add(emp2);
-		theEmployees.add(emp3);
+	@Autowired
+	public EmployeeController(EmployeeService employeeService) {
+		this.employeeService = employeeService;
 	}
 	
 	// add mapping for "/list"
 	@GetMapping("/list")
 	public String listEmployees(Model theModel) {
 		
-		theModel.addAttribute("employees", theEmployees);
+		theModel.addAttribute("employees", employeeService.findAll());
 		return "list-employees";
 	}
 }
