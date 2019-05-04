4e9e6804ce9f20c168105c80f5a4c0fd26a70630
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-05-03 21:38:45 -0400

    Add a method to interface Employee Repository to sort with lastName

--- Spring_Boot_Rest-CRUD_Thymeleaf/src/main/java/com/luv2code/springboot/thymeleafdemo/dao/EmployeeRepository.java
+++ Spring_Boot_Rest-CRUD_Thymeleaf/src/main/java/com/luv2code/springboot/thymeleafdemo/dao/EmployeeRepository.java
@@ -1,9 +1,14 @@
 package com.luv2code.springboot.thymeleafdemo.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.luv2code.springboot.thymeleafdemo.entity.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 	
+	// add a method to sort by last name - create a special method by SprintDataJpa by parsing the name
+	// looks for a specific format and patter : Creates appropriate query ... behind the scenes
+	public List<Employee> findAllByOrderByLastNameAsc();
 }
