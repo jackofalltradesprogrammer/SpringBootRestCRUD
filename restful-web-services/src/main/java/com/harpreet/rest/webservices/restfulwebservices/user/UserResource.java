174f323125e6940be1518beca6cff618b04a24b1
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-05-10 16:51:39 -0400

    Return "201" code for successful creation with URI in the header

--- restful-web-services/src/main/java/com/harpreet/rest/webservices/restfulwebservices/user/UserResource.java
+++ restful-web-services/src/main/java/com/harpreet/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,13 +1,16 @@
 package com.harpreet.rest.webservices.restfulwebservices.user;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 public class UserResource {
@@ -27,10 +30,17 @@ public class UserResource {
 	public User retrieveUser(@PathVariable int id) {
 		return service.findOne(id);
 	}
-	
+
+	// input - details of user
+	// output - CREATE & Return the created URI
 	@PostMapping("/users")
-	public void createUser(@RequestBody User user) {
+	public ResponseEntity<Object> createUser(@RequestBody User user) {
 		// @RequestBody tries to map whatever is coming in request to User
 		User savedUser = service.save(user);
+		// CREATED - Return the status code with URI
+		// /users/{id}    savedUser.getId()
+		// to get current URI use ServletUriComponentsBuilder
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
+		return ResponseEntity.created(location).build();
 	}
 }
