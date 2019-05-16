c389107d9ceeac8f26e2f4ccba0463c34bfe78e1
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-05-16 15:32:59 -0400

    Simplification (removing extra configurations) with spring boot

--- restful-web-services/src/main/java/com/harpreet/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ restful-web-services/src/main/java/com/harpreet/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 
 @SpringBootApplication
 public class RestfulWebServicesApplication {
@@ -19,16 +19,17 @@ public class RestfulWebServicesApplication {
 	// Configuration for the Internationalization 
 	@Bean
 	public LocaleResolver localeResolver() {
-		SessionLocaleResolver localeResolver = new SessionLocaleResolver();
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
 		localeResolver.setDefaultLocale(Locale.US);
 		return localeResolver;
 	}
 	
 	// To read properties files 
-	@Bean
-	public ResourceBundleMessageSource messageSource() {
-		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
-		messageSource.setBasename("messages");
-		return messageSource;
-	}
+	// we can just update application.properties for the springboot
+//	@Bean
+//	public ResourceBundleMessageSource messageSource() {
+//		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+//		messageSource.setBasename("messages");
+//		return messageSource;
+//	}
 }
--- restful-web-services/src/main/java/com/harpreet/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ restful-web-services/src/main/java/com/harpreet/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,12 +1,10 @@
 package com.harpreet.rest.webservices.restfulwebservices.helloworld;
 
-import java.util.Locale;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -35,8 +33,9 @@ public class HelloWorldController {
 	}
 	
 	// to display "hello world" in locale language, it gets the 'locale' from header
+	// Spring provides an alternative so that we don't have to hard-code locale
 	@GetMapping("/hello-world-internationalized")
-	public String helloWorldInternationalized(@RequestHeader(name="Accept-Language", required = false) Locale locale) {
-		return messageSource.getMessage("good.morning.message", null, locale);
+	public String helloWorldInternationalized() {
+		return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
 	}
 }
