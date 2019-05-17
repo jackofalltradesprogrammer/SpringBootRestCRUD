665a71b1eaab9e8419fd7e287cd0ca798e0253a6
Harpreet Singh <jackofalltradesprogrammer@gmail.com>
2019-05-17 12:52:09 -0400

    Enhancing Swagger documentation with custom Annotations

--- restful-web-services/src/main/java/com/harpreet/rest/webservices/restfulwebservices/SwaggerConfig.java
+++ restful-web-services/src/main/java/com/harpreet/rest/webservices/restfulwebservices/SwaggerConfig.java
@@ -1,8 +1,14 @@
 package com.harpreet.rest.webservices.restfulwebservices;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -12,13 +18,26 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 //Enable Swagger
 @EnableSwagger2
 public class SwaggerConfig {
-	
+
+	// to configre the contact info and other details of the api
+	public static final Contact DEFAULT_CONTACT = new Contact(
+			"Ranga Karanam", "http://www.in28minutes.com", "http://www.in28minutes.com");
+	public static final ApiInfo DEFAULT_API_INFO = new ApiInfo(
+			"Awesome API Title", "Awesome API Documentation", "1.0", "urn:tos",
+			DEFAULT_CONTACT, "Apache 2.0", "http://www.apache.org/licenses/LICENSE-2.0");
+	// provide info what the web service produces and consumes
+	private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = 
+			new HashSet<String>(Arrays.asList("application/json","application/xml"));
+
 	@Bean
 	public Docket api() {
-		return new Docket(DocumentationType.SWAGGER_2);
+		return new Docket(DocumentationType.SWAGGER_2)
+				.apiInfo(DEFAULT_API_INFO)
+				.produces(DEFAULT_PRODUCES_AND_CONSUMES)
+				.consumes(DEFAULT_PRODUCES_AND_CONSUMES);
 	}
-	//Bean - Docket
-	// Swagger 2 
+	// Bean - Docket
+	// Swagger 2
 	// All the paths
 	// All the apis
 }
